4db178984634856ebd77b3602decbd6d0009a860
aba-geomdan <abageomdan@gmail.com>
2026-07-14 10:56:19 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import obfuscator from "vite-plugin-javascript-obfuscator";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscator({
+      apply: "build",
+      include: [/\.js$/],
+      exclude: [/node_modules/],
+      debugger: false,
+      options: {
+        compact: true,
+        stringArray: true,
+        stringArrayEncoding: ["base64"],
+        stringArrayThreshold: 0.75,
+        identifierNamesGenerator: "hexadecimal",
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        selfDefending: false,
+      },
+    }),
+  ],
   base: "/geomdan-ceu/",
 });
